69fd658a301192ca86ce0b88ef9b1960dede7f82
ViewDidLoad <viewdidload@gmail.com>
2020-03-05 07:06:33 +0900

    Initial Commit

---
+++ fileMusic/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  fileMusic
+//
+//  Created by viewdidload soft on 2020/03/05.
+//  Copyright © 2020 viewdidload soft. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
