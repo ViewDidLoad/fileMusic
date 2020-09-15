476079e53937f29324e2ea62ad4a2a3e5a1c4b8a
ViewDidLoad <viewdidload@gmail.com>
2020-09-15 15:00:18 +0900

    스플래쉬 화면에 제목 라벨 추가

---
+++ fileMusic/SplashViewController.swift
@@ -0,0 +1,39 @@
+//
+//  SplashViewController.swift
+//  fileMusic
+//
+//  Created by viewdidload soft on 2020/09/15.
+//  Copyright © 2020 viewdidload soft. All rights reserved.
+//
+
+import UIKit
+
+class SplashViewController: UIViewController {
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var titleTopConstraint: NSLayoutConstraint!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        titleTopConstraint.constant = 10
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        titleTopConstraint.constant = UIScreen.main.bounds.midY
+        UIView.animate(withDuration: 0.3) {
+            self.view.layoutIfNeeded()
+        }
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        // 지연 실행
+        DispatchQueue.main.asyncAfter(deadline: .now() + .seconds(1)) {
+            let board = UIStoryboard(name: "Main", bundle: nil)
+            let vc = board.instantiateViewController(withIdentifier: "mainVC") as! MainViewController
+            vc.modalPresentationStyle = .fullScreen
+            vc.modalTransitionStyle = .crossDissolve
+            self.present(vc, animated: false, completion: nil)
+        }
+    }
+}
