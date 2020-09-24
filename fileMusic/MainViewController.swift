d012298cee761728c989cdbc4f174a58ff3399ff
ViewDidLoad <viewdidload@gmail.com>
2020-09-24 13:07:30 +0900

    테이블 뷰의 셀 드래그 드롭 구현

--- fileMusic/MainViewController.swift
+++ fileMusic/MainViewController.swift
@@ -27,7 +27,7 @@ class MainViewController: UIViewController {
     // 플레이 파일 목록
     let fm = FileManager.default
     var docuPath = ""
-    var data_item = [String]()
+    var data_item = [URL]()
     // 음원 플레이어
     let engine = AVAudioEngine()
     let player = AVAudioPlayerNode()
@@ -39,13 +39,26 @@ class MainViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // 파일 설정
+        // 파일 설정 -> 리스트에 url을 넣자
         docuPath = fm.urls(for: .documentDirectory, in: .userDomainMask).first!.path
         do {
             // 접근한 경로의 디렉토리 내 파일 리스트를 불러옵니다.
             let items = try fm.contentsOfDirectory(atPath: docuPath)
-            for item in items { data_item.append(item) }
+            for item in items {
+                let filename = "\(docuPath)/\(item)"
+                let fileUrl = URL(fileURLWithPath: filename)
+                data_item.append(fileUrl)
+            }
         } catch { print("Not Found item") }
+        // 가져올 파일이 없으면 샘플 파일을 로딩
+        if data_item.count == 0 {
+            if let fileUrl_1 = Bundle.main.url(forResource: "Blumenlied", withExtension: "mp3") {
+                data_item.append(fileUrl_1)
+            }
+            if let fileUrl_2 = Bundle.main.url(forResource: "Canon", withExtension: "mp3") {
+                data_item.append(fileUrl_2)
+            }
+        }
         // engine 설정
         engine.attach(player)
         engine.attach(reverb)
@@ -70,17 +83,20 @@ class MainViewController: UIViewController {
             return .success
         }
         commandCenter.nextTrackCommand.addTarget { [unowned self] event in
-            self.nextPlay()
+            //self.nextPlay()
             return .success
         }
         commandCenter.previousTrackCommand.addTarget { [unowned self] event in
-            self.prevPlay()
+            //self.prevPlay()
             return .success
         }
         // tableView
-        fileListTableView.rowHeight = UITableView.automaticDimension
         fileListTableView.delegate = self
         fileListTableView.dataSource = self
+        fileListTableView.rowHeight = UITableView.automaticDimension
+        fileListTableView.dragDelegate = self
+        fileListTableView.dropDelegate = self
+        fileListTableView.dragInteractionEnabled = true
         // 알람 설정 - 오디오 중단 발생 알림
         NotificationCenter.default.addObserver(self, selector: #selector(handleInterruption), name: AVAudioSession.interruptionNotification, object: nil)
         // 알람 설정 - 해드폰에서 스피커 등 변경될 때
@@ -88,12 +104,9 @@ class MainViewController: UIViewController {
     }
     
     override func viewWillAppear(_ animated: Bool) {
-        // 이름 순으로 정렬
-        data_item.sort()
         DispatchQueue.main.async {
             self.fileListTableView.reloadData()
         }
-        
         // 타이머 설정 - 프로그래스 바에 얼마만큼 플레이 중인지 표시
         let timer = Timer.scheduledTimer(withTimeInterval: 0.3, repeats: true, block: { [unowned self] (timer) in
             // 플레이어 플레이 중일 때만
@@ -124,110 +137,24 @@ class MainViewController: UIViewController {
             musicPlay(music: data_item[selectIndex])
         }
     }
-
-    func playReset() {
-        // 새롭게 설정해야 플레이 상태가 초기화 됨
-        player.reset()
-//        engine.attach(player)
-//        engine.attach(reverb)
-//        engine.connect(player, to: reverb, format: format)
-//        engine.connect(reverb, to: engine.mainMixerNode, format: format)
-        
-        let playMusicTitle = data_item.count > 0 ? data_item[selectIndex] : "sample.mp3"
-        DispatchQueue.main.async {
-            self.playTitleLabel.text = playMusicTitle
-        }
-    }
     
-    func play() {
-        if data_item.count > 0 {
-            // 가져온 파일이 있으면 리스트로 플레이
-            let music_name = data_item[selectIndex]
-            let filename = "\(docuPath)/\(music_name)"
-            print("\(music_name) start")
-            let fileUrl = URL(fileURLWithPath: filename)
-            do {
-                sourceFile = try AVAudioFile(forReading: fileUrl)
-                if let audio_file = sourceFile {
-                    format = audio_file.processingFormat
-                    player.scheduleFile(audio_file, at: nil, completionHandler: {
-                        print("\(music_name) completed")
-                        self.nextPlay()
-                    })
-                    setupRemoteCommand(title: music_name, current: player.current, duration: audio_file.duration, rate: player.rate)
-                    player.play()
-                }
-            } catch { print("AVAudioFile error -> \(error.localizedDescription)") }
-        } // 가저온 파일이 없으면 샘플로 플레이 한다.
-        else {
-            guard let sampleUrl = Bundle.main.url(forResource: "sample", withExtension: "mp3") else { return }
-            do {
-                sourceFile = try AVAudioFile(forReading: sampleUrl)
-                if let audio_file = sourceFile {
-                    format = audio_file.processingFormat
-                    player.scheduleFile(audio_file, at: nil, completionHandler: {
-                        print("sample.mp3 completed")
-                    })
-                    setupRemoteCommand(title: "sample.mp3", current: player.current, duration: audio_file.duration, rate: player.rate)
-                    self.playTitleLabel.text = "sample.mp3"
-                    player.play()
-                }
-            } catch { print("AVAudioFile error -> \(error.localizedDescription)") }
-        }
-    }
-    
-    func nextPlay() {
-        if data_item.count > 0 {
-            // 파일 리스트가 있을 경우
-            selectIndex += 1
-            if selectIndex >= data_item.count { selectIndex = 0 }
-            // 테이블 셀 선택 바꿔줘야 함
-            let indexPath = IndexPath(row: selectIndex, section: 0)
-            DispatchQueue.main.async {
-                self.fileListTableView.selectRow(at: indexPath, animated: true, scrollPosition: UITableView.ScrollPosition.middle)
-            }
-            playReset()
-            // 0.5초후에 플레이
-            DispatchQueue.main.asyncAfter(deadline: .now() + .microseconds(500)) {
-                self.play()
-            }
-        }
-    }
-    
-    func prevPlay() {
-        if data_item.count > 0 {
-            // 파일 리스트가 있을 경우
-            selectIndex -= 1
-            if selectIndex < 0 { selectIndex = data_item.count - 1 }
-            // 테이블 셀 선택 바꿔줘야 함
-            let indexPath = IndexPath(row: selectIndex, section: 0)
-            DispatchQueue.main.async {
-                self.fileListTableView.selectRow(at: indexPath, animated: true, scrollPosition: UITableView.ScrollPosition.middle)
-            }
-            playReset()
-            // 0.5초후에 플레이
-            DispatchQueue.main.asyncAfter(deadline: .now() + .microseconds(500)) {
-                self.play()
-            }
-        }
-    }
-    
-    func musicPlay(music: String) {
+    func musicPlay(music: URL) {
         // 실행되고 있으면 중단
         if player.isPlaying { player.stop() }
-        let filename = "\(docuPath)/\(music)"
-        let fileUrl = URL(fileURLWithPath: filename)
-        do {
-            let audio_file = try AVAudioFile(forReading: fileUrl)
-            sourceFile = audio_file
-            format = audio_file.processingFormat
-            player.scheduleFile(audio_file, at: nil, completionHandler: {
-                print("\(music) completed")
-            })
-            playTitleLabel.text = music
-            setupRemoteCommand(title: music, current: player.current, duration: audio_file.duration, rate: player.rate)
-            player.play()
-        } catch { print("AVAudioFile error -> \(error.localizedDescription)") }
+        print("musicPlay \(music)")
+        if let name = music.absoluteString.split(separator: "/").last?.split(separator: ".").first {
+            do {
+                let audio_file = try AVAudioFile(forReading: music)
+                sourceFile = audio_file
+                format = audio_file.processingFormat
+                player.scheduleFile(audio_file, at: nil, completionHandler: {
+                    print("\(name) completed")
+                })
+                playTitleLabel.text = String(name)
+                setupRemoteCommand(title: String(name), current: player.current, duration: audio_file.duration, rate: player.rate)
+                player.play()
+            } catch { print("AVAudioFile error -> \(error.localizedDescription)") }
+        }
     }
     
     func hasHeadphones(in routeDescription: AVAudioSessionRouteDescription) -> Bool {
@@ -290,18 +217,6 @@ extension MainViewController: UITableViewDelegate {
     func tableView(_ tableView: UITableView, estimatedHeightForRowAt indexPath: IndexPath) -> CGFloat {
         return UITableView.automaticDimension
     }
-}
-
-extension MainViewController: UITableViewDataSource {
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return data_item.count
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "fileCell", for: indexPath) as! FileListCell
-        cell.fileTitleLabel.text = data_item[indexPath.row]
-        return cell
-    }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         // 선택된 인덱스 저장
@@ -312,6 +227,18 @@ extension MainViewController: UITableViewDataSource {
         DispatchQueue.main.asyncAfter(deadline: .now() + .microseconds(300)) {
             self.playButton.setImage(UIImage(named: "icon_pause"), for: .normal)
         }
+    }
+    
+    func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        print("canMoveRowAt")
+        return true
+    }
+    
+    func tableView(_ tableView: UITableView, moveRowAt sourceIndexPath: IndexPath, to destinationIndexPath: IndexPath) {
+        print("moveRowAt")
+        let tmp = data_item[sourceIndexPath.row]
+        data_item.remove(at: sourceIndexPath.row)
+        data_item.insert(tmp, at: destinationIndexPath.row)
     }
     
     func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
@@ -333,5 +260,43 @@ extension MainViewController: UITableViewDataSource {
         let configuration = UISwipeActionsConfiguration(actions: [action])
         return configuration
     }
+    
 }
 
+extension MainViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return data_item.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "fileCell", for: indexPath) as! FileListCell
+        if let name = data_item[indexPath.row].absoluteString.split(separator: "/").last?.split(separator: ".").first {
+            cell.fileTitleLabel.text = String(name)
+        }
+        return cell
+    }
+}
+
+extension MainViewController: UITableViewDragDelegate {
+    func tableView(_ tableView: UITableView, itemsForBeginning dragSession: UIDragSession, at indexPath: IndexPath) -> [UIDragItem] {
+        print("itemsForBeginning")
+        let music = self.data_item[indexPath.row]
+        let itemProvider = NSItemProvider(object: music as NSItemProviderWriting)
+        let dragItem = UIDragItem(itemProvider: itemProvider)
+        return [dragItem]
+    }
+    
+}
+
+extension MainViewController: UITableViewDropDelegate {
+    func tableView(_ tableView: UITableView, dropSessionDidUpdate session: UIDropSession, withDestinationIndexPath destinationIndexPath: IndexPath?) -> UITableViewDropProposal {
+        //print("dropSessionDidUpdate")
+        // 움직일때마다 이걸 탄다, 좌표마다 타는 것 같음
+        return UITableViewDropProposal(operation: .move, intent: .insertAtDestinationIndexPath)
+    }
+    
+    func tableView(_ tableView: UITableView, performDropWith coordinator: UITableViewDropCoordinator) {
+        print("performDropWith")
+        // 확인해보니 현재 이곳을 타지 않는다.
+    }
+}
