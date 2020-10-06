38392be1054c403fb2798c4b51f6eda1cf591424
ViewDidLoad <viewdidload@gmail.com>
2020-10-07 08:01:36 +0900

    동영상 플레이 오류 추적 중

--- fileMusic/ConfigViewController.swift
+++ fileMusic/ConfigViewController.swift
@@ -27,6 +27,11 @@ class ConfigViewController: UIViewController {
         NotificationCenter.default.addObserver(self, selector: #selector(playerDidFinishPlaying(_:)), name: .AVPlayerItemDidPlayToEndTime, object: nil)
     }
     
+    override func viewWillDisappear(_ animated: Bool) {
+        // 노티 감시 제거
+        NotificationCenter.default.removeObserver(self, name: .AVPlayerItemDidPlayToEndTime, object: nil)
+    }
+    
     @IBAction func closeButtonTouched(_ sender: UIButton) {
         print("closeButtonTouched")
         // 메인 창으로 이동
@@ -39,6 +44,17 @@ class ConfigViewController: UIViewController {
     
     @IBAction func playButtonTouched(_ sender: UIButton) {
         print("playButtonTouched")
+        // 파일을 못 가져옴, 왜 그럴까?
+        if let fileurl = Bundle.main.url(forResource: "fileMusicDemoSmall", withExtension: "mp4") {
+            let player = AVPlayer(url: fileurl)
+            let playerLayer = AVPlayerLayer(player: player)
+            playerLayer.videoGravity = .resize
+            playerLayer.frame = sender.frame
+            playerLayer.name = fileurl.deletingPathExtension().lastPathComponent
+            movieView.layer.addSublayer(playerLayer)
+            player.play()
+        }
+        
     }
     
     @objc func playerDidFinishPlaying(_ noti: Notification) {
