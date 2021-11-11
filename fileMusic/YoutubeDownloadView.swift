a1b0caf9895ab976b3f24e0276d44972891c781c
whenyourapprun <whenyourapprun@gmail.com>
2021-11-11 18:43:08 +0900

    체크 다운로드 함수로 분기

--- fileMusic/YoutubeDownloadView.swift
+++ fileMusic/YoutubeDownloadView.swift
@@ -15,7 +15,7 @@ struct YoutubeDownloadView: View {
     @State var isDownloadButtonDisable = true
     @State var isCheckButtonDisable = true
     @State var state = "Status "
-    let checkTimer = Timer.publish(every: 5, on: .main, in: .common).autoconnect()
+    let checkTimer = Timer.publish(every: 8, on: .main, in: .common).autoconnect()
     
     var body: some View {
         List {
@@ -41,63 +41,47 @@ struct YoutubeDownloadView: View {
             Text(state)
             Button("Download Check") {
                 print("Download Check clicked")
-                checkURL(check_url: download_url) { msg in
-                    print("checkURL \(msg.result)")
-                    if msg.result != "" {
-                        state = "Status : downloading..."
-                        isCheckButtonDisable = true
-                        //*/ 서버에 요청해서 파일을 다운받아서 도큐먼트에 저장하자.
-                        getFileData(filename: msg.result) { msg in
-                            print("getFileData \(msg.result)")
-                            state = "Status : download completed."
-                            // 몇 초 후에 창을 닫자.
-                            DispatchQueue.main.asyncAfter(deadline: .now() + .microseconds(1500)) {
-                                // 현재 창을 닫자.
-                                self.presentationMode.wrappedValue.dismiss()
-                                // 다운로드 된 파일 갱신
-                                NotificationCenter.default.post(name: Notification.Name("updateFileList"), object: nil)
-                            }
-                        }
-                        // */
-                    } else {
-                        state = "Status : ready download configuration"
-                    }
-                }
+                checkDownload()
             }
             .disabled(isCheckButtonDisable)
             .onReceive(checkTimer) { input in
                 print("checkTimer \(input)")
                 // 체크 버튼이 활성화 될 때 체크하자.
                 if isCheckButtonDisable == false {
-                    checkURL(check_url: download_url) { msg in
-                        print("checkURL \(msg.result)")
-                        if msg.result != "" {
-                            state = "Status : downloading..."
-                            isCheckButtonDisable = true
-                            // 타이머 중지
-                            checkTimer.upstream.connect().cancel()
-                            //*/ 서버에 요청해서 파일을 다운받아서 도큐먼트에 저장하자.
-                            getFileData(filename: msg.result) { msg in
-                                print("getFileData \(msg.result)")
-                                state = "Status : download completed."
-                                // 몇 초 후에 창을 닫자.
-                                DispatchQueue.main.asyncAfter(deadline: .now() + .microseconds(1500)) {
-                                    // 현재 창을 닫자.
-                                    self.presentationMode.wrappedValue.dismiss()
-                                    // 다운로드 된 파일 갱신
-                                    NotificationCenter.default.post(name: Notification.Name("updateFileList"), object: nil)
-                                }
-                            }
-                            // */
-                        } else {
-                            state = "Status : ready download configuration"
-                        }
-                    }
+                    checkDownload()
                 } // end_if isCheckButtonDisable = false {
             }
         }
-        
     }
+    
+    // 다운로드 상태 체크
+    func checkDownload() {
+        checkURL(check_url: download_url) { msg in
+            print("checkURL \(msg.result)")
+            if msg.result != "" {
+                state = "Status : downloading..."
+                isCheckButtonDisable = true
+                // 타이머 중지
+                checkTimer.upstream.connect().cancel()
+                //*/ 서버에 요청해서 파일을 다운받아서 도큐먼트에 저장하자.
+                getFileData(filename: msg.result) { msg in
+                    print("getFileData \(msg.result)")
+                    state = "Status : download completed."
+                    // 몇 초 후에 창을 닫자.
+                    DispatchQueue.main.asyncAfter(deadline: .now() + .microseconds(1500)) {
+                        // 현재 창을 닫자.
+                        self.presentationMode.wrappedValue.dismiss()
+                        // 다운로드 된 파일 갱신
+                        NotificationCenter.default.post(name: Notification.Name("updateFileList"), object: nil)
+                    }
+                }
+                // */
+            } else {
+                state = "Status : ready download configuration"
+            }
+        }
+    }
+    
 }
 
 struct YoutubeDownloadView_Previews: PreviewProvider {
