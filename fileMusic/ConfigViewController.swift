b0735757968997b618525aea2c1622223ac48fab
whenyourapprun <whenyourapprun@gmail.com>
2021-11-12 04:12:45 +0900

    설정창의 동영상 안나오는 문제 수정

--- fileMusic/ConfigViewController.swift
+++ fileMusic/ConfigViewController.swift
@@ -114,11 +114,10 @@ class ConfigViewController: UIViewController {
     @IBAction func playButtonTouched(_ sender: UIButton) {
         print("playButtonTouched")
         // 파일을 못 가져옴, 왜 그럴까? 찾았다. build phases - bundle resources 에 이 파일이 없어서 그럼, 추가하니 잘나옴
-        //if let url = Bundle.main.url(forResource: "fileMusicDemoSmall", withExtension: "mp4") {
-        if let url = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first {
+        if let url = Bundle.main.url(forResource: "fileMusicDemoSmall", withExtension: "mp4") {
             let player = AVPlayer(url: url)
             let playerLayer = AVPlayerLayer(player: player)
-            playerLayer.videoGravity = .resize
+            playerLayer.videoGravity = .resizeAspect
             playerLayer.frame = sender.frame
             playerLayer.name = url.deletingPathExtension().lastPathComponent
             movieView.layer.addSublayer(playerLayer)
