94b6c0ee3743491a4815932973dfa0e69e5accdf
whenyourapprun <whenyourapprun@gmail.com>
2021-11-11 18:24:59 +0900

    타이머로 5초마다 체크 기능 추가

--- fileMusic/YoutubeDownloadView.swift
+++ fileMusic/YoutubeDownloadView.swift
@@ -15,6 +15,7 @@ struct YoutubeDownloadView: View {
     @State var isDownloadButtonDisable = true
     @State var isCheckButtonDisable = true
     @State var state = "Status "
+    let checkTimer = Timer.publish(every: 5, on: .main, in: .common).autoconnect()
     
     var body: some View {
         List {
@@ -64,6 +65,34 @@ struct YoutubeDownloadView: View {
                 }
             }
             .disabled(isCheckButtonDisable)
+            .onReceive(checkTimer) { input in
+                print("checkTimer \(input)")
+                // 체크 버튼이 활성화 될 때 체크하자.
+                if isCheckButtonDisable == false {
+                    checkURL(check_url: download_url) { msg in
+                        print("checkURL \(msg.result)")
+                        if msg.result != "" {
+                            state = "Status : downloading..."
+                            isCheckButtonDisable = true
+                            //*/ 서버에 요청해서 파일을 다운받아서 도큐먼트에 저장하자.
+                            getFileData(filename: msg.result) { msg in
+                                print("getFileData \(msg.result)")
+                                state = "Status : download completed."
+                                // 몇 초 후에 창을 닫자.
+                                DispatchQueue.main.asyncAfter(deadline: .now() + .microseconds(1500)) {
+                                    // 현재 창을 닫자.
+                                    self.presentationMode.wrappedValue.dismiss()
+                                    // 다운로드 된 파일 갱신
+                                    NotificationCenter.default.post(name: Notification.Name("updateFileList"), object: nil)
+                                }
+                            }
+                            // */
+                        } else {
+                            state = "Status : ready download configuration"
+                        }
+                    }
+                } // end_if isCheckButtonDisable = false {
+            }
         }
         
     }
