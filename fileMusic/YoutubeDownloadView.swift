7cf60b016b8c9a72b0c682ddf637a40825f06b96
whenyourapprun <whenyourapprun@gmail.com>
2021-11-11 19:57:16 +0900

    로딩 화면 밑에 정보 보이도록 투명도 및 블루어 조정

--- fileMusic/YoutubeDownloadView.swift
+++ fileMusic/YoutubeDownloadView.swift
@@ -14,43 +14,46 @@ struct YoutubeDownloadView: View {
     @State var download_url = ""//"https://youtu.be/-n_Kw19q2bM" <- 첨밀밀
     @State var isDownloadButtonDisable = true
     @State var isCheckButtonDisable = true
+    @State var isLoadingViewEnable = false
     @State var state = "Status "
-    let checkTimer = Timer.publish(every: 8, on: .main, in: .common).autoconnect()
+    let checkTimer = Timer.publish(every: 9, on: .main, in: .common).autoconnect()
     
     var body: some View {
-        List {
-            Text("YouTube Download")
-            TextField("input youtube url", text: $download_url, onEditingChanged: { _ in
-                print("onEditingChanged")
-            }, onCommit: {
-                print("input url completed")
-                // 공백이 아닐 때 버튼 활성화
-                isDownloadButtonDisable = download_url == "" ? true : false
-            })
-            Button("Download URL") {
-                print("Download URL clicked")
-                // 여기서 서버에 요청을 하자.
-                downloadURL(download_url: download_url) { msg in
-                    print("downloadURL \(msg.result)")
-                    state = "Status : start download"
-                    isDownloadButtonDisable = true
-                    isCheckButtonDisable = false
+        LoadingView(isShowing: .constant(isLoadingViewEnable)) {
+            List {
+                Text("YouTube Download")
+                TextField("input youtube url", text: $download_url, onEditingChanged: { _ in
+                    print("onEditingChanged")
+                }, onCommit: {
+                    print("input url completed")
+                    // 공백이 아닐 때 버튼 활성화
+                    isDownloadButtonDisable = download_url == "" ? true : false
+                })
+                Button("Download URL") {
+                    print("Download URL clicked")
+                    // 여기서 서버에 요청을 하자.
+                    downloadURL(download_url: download_url) { msg in
+                        print("downloadURL \(msg.result)")
+                        state = "Status : start download"
+                        isDownloadButtonDisable = true
+                        isCheckButtonDisable = false
+                        isLoadingViewEnable = true
+                    }
                 }
-            }
-            .disabled(isDownloadButtonDisable)
-            Text(state)
-            Button("Download Check") {
-                print("Download Check clicked")
-                checkDownload()
-            }
-            .disabled(isCheckButtonDisable)
-            .onReceive(checkTimer) { input in
-                print("checkTimer \(input)")
-                // 체크 버튼이 활성화 될 때 체크하자.
-                if isCheckButtonDisable == false {
+                .disabled(isDownloadButtonDisable)
+                Text(state)
+                Button("Download Check") {
                     checkDownload()
-                } // end_if isCheckButtonDisable = false {
-            }
+                }
+                .disabled(isCheckButtonDisable)
+                .onReceive(checkTimer) { input in
+                    //print("checkTimer \(input)")
+                    // 체크 버튼이 활성화 될 때 체크하자.
+                    if isCheckButtonDisable == false {
+                        checkDownload()
+                    } // end_if isCheckButtonDisable = false {
+                }
+            } // end_List
         }
     }
     
@@ -59,14 +62,16 @@ struct YoutubeDownloadView: View {
         checkURL(check_url: download_url) { msg in
             print("checkURL \(msg.result)")
             if msg.result != "" {
-                state = "Status : downloading..."
+                state = "Status : Downloading..."
                 isCheckButtonDisable = true
                 // 타이머 중지
                 checkTimer.upstream.connect().cancel()
+                // 로딩뷰 감추기
+                isLoadingViewEnable = false
                 //*/ 서버에 요청해서 파일을 다운받아서 도큐먼트에 저장하자.
                 getFileData(filename: msg.result) { msg in
                     print("getFileData \(msg.result)")
-                    state = "Status : download completed."
+                    state = "Status : Download Completed."
                     // 몇 초 후에 창을 닫자.
                     DispatchQueue.main.asyncAfter(deadline: .now() + .microseconds(1500)) {
                         // 현재 창을 닫자.
@@ -77,11 +82,51 @@ struct YoutubeDownloadView: View {
                 }
                 // */
             } else {
-                state = "Status : ready download configuration"
+                state = "Status : Downloading"
             }
         }
     }
     
+}
+
+struct ActivityIndicator: UIViewRepresentable {
+    @Binding var isAnimating: Bool
+    let style: UIActivityIndicatorView.Style
+
+    func makeUIView(context: UIViewRepresentableContext<ActivityIndicator>) -> UIActivityIndicatorView {
+        return UIActivityIndicatorView(style: style)
+    }
+
+    func updateUIView(_ uiView: UIActivityIndicatorView, context: UIViewRepresentableContext<ActivityIndicator>) {
+        isAnimating ? uiView.startAnimating() : uiView.stopAnimating()
+    }
+}
+
+struct LoadingView<Content>: View where Content: View {
+    @Binding var isShowing: Bool
+    var content: () -> Content
+
+    var body: some View {
+        GeometryReader { geometry in
+            ZStack(alignment: .center) {
+                self.content()
+                    .disabled(self.isShowing)
+                    .blur(radius: self.isShowing ? 1 : 0)
+
+                VStack {
+                    Text("Downloading...")
+                    ActivityIndicator(isAnimating: .constant(true), style: .large)
+                }
+                .frame(width: geometry.size.width / 2,
+                       height: geometry.size.height / 5)
+                .background(Color.secondary.colorInvert())
+                .foregroundColor(Color.primary)
+                .cornerRadius(20)
+                .opacity(self.isShowing ? 0.7 : 0)
+
+            }
+        }
+    }
 }
 
 struct YoutubeDownloadView_Previews: PreviewProvider {
